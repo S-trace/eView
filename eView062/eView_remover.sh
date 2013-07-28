2c47fc2375450e44a41ca670c12c890db825f6cc
S-trace <S-trace@list.ru>
2013-07-29 03:23:15 +0400

    062 release

---
+++ eView062/eView_remover.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+PRGNAME=eView
+rm /usr/bin/$PRGNAME /usr/share/applications/$PRGNAME.desktop /usr/local/share/applications/${PRGNAME}.desktop /usr/share/filemanager/pixmaps/desktop_$PRGNAME.png /usr/local/share/desktop/icons/$PRGNAME.png
+echo '[Desktop Entry]
+Name=Manual
+GenericName=Manual
+Exec=help.sh
+Type=Application
+Icon=manual
+Name[ru_RU]=Руководство
+' > /usr/local/share/applications/manual.desktop
