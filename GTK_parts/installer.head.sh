3a65a41a5f5fb525eec4f5b155543e8af0267c27
S-trace <S-trace@list.ru>
2013-08-21 18:43:34 +0400

    Create fontconfig cache during installation

--- GTK_parts/installer.head.sh
+++ GTK_parts/installer.head.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=3
+version=4
 echo "Installing GTK_parts_V$version"
 echo "\$0 is '$0'"
 self_name=`readlink -f $0`
@@ -8,6 +8,7 @@ self_path=`dirname $self_name`
 echo "\$self_path is '$self_path'"
 echo "pwd is `pwd`"
 PRGNAME=GTK_parts
+dbus-send --type=method_call --dest=com.sibrary.BoeyeServer /StartupSplash com.sibrary.Service.StartupSplash.showSplash
 rm -rf $self_path/temp_$PRGNAME
 mkdir -p $self_path/temp_$PRGNAME
 cd $self_path/temp_$PRGNAME
@@ -20,8 +21,14 @@ cd -
 gdk-pixbuf-query-loaders > /etc/gtk-2.0/gdk-pixbuf.loaders
 pango-querymodules > '/etc/pango/pango.modules'
 rm -rf $self_path/temp.tar $self_path/temp_$PRGNAME
-echo "Message: GTK_parts version $version installed" > /tmp/message.txt
 echo $version > /home/root/.GTK_parts.version
+
+Xfbdev :0 -br -pn -hide-cursor -dpi 150 -rgba vrgb &
+messagebox "Installing GTK_parts version $version" & # На самом деле важная штука, во время этого вывода строится кэш fontconfig!
+until [[ -f /home/root/.fontconfig/* ]]; do sleep 1 ; done
+killall Xfbdev
+dbus-send --type=method_call --dest=com.sibrary.BoeyeServer /StartupSplash com.sibrary.Service.StartupSplash.closeSplash
+echo "Message: GTK_parts version $version installed" > /tmp/message.txt
 dbus-send --type=method_call --dest=com.test.reader /reader/registry com.test.reader.registry.input string:"/tmp/message.txt"
 exit 0
 __DATA__
