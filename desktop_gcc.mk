5a4c602ef2dbc3f1f6ed72d86f258b34e0a018fb
S-trace <S-trace@list.ru>
2015-01-27 01:16:07 +0300

    Fix build failure on desktop GCC build

--- desktop_gcc.mk
+++ desktop_gcc.mk
@@ -1,5 +1,5 @@
 CC = gcc
 LD = gcc
 CFLAGS += $(shell pkg-config --cflags gtk+-2.0)
-CFLAGS += $(DFLAGS) -g -Ddebug_printf -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wsuggest-attribute=pure -Wsuggest-attribute=const -Wsuggest-attribute=noreturn -fipa-pure-const -Wlogical-op -Wcast-align #-Wconversion
+CFLAGS += $(DFLAGS) -g -Ddebug_printf -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wsuggest-attribute=const -Wsuggest-attribute=noreturn -fipa-pure-const -Wlogical-op -Wcast-align #-Wconversion
 LDFLAGS += $(shell pkg-config --libs gtk+-2.0) -rdynamic
