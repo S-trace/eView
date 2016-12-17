b151a0f6e6de54e10a6ebfaed67679574b3484cd
S-trace <S-trace@list.ru>
2016-12-17 21:29:38 +0300

    [MAKEFILE] Fix debug building cflag for PC builds

--- desktop_clang.mk
+++ desktop_clang.mk
@@ -1,5 +1,5 @@
 CC = clang
 LD = clang
 CFLAGS += $(shell pkg-config --cflags gtk+-2.0)
-CFLAGS += $(DFLAGS) -g -Ddebug_printf  -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wcast-align #-Wconversion
+CFLAGS += $(DFLAGS) -g -Ddebug  -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wcast-align #-Wconversion
 LDFLAGS += $(shell pkg-config --libs gtk+-2.0) -rdynamic
