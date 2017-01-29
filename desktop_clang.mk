5540e545d90c53a6b4e79a6b5f5ad2dd1dae1afb
S-trace <S-trace@list.ru>
2017-01-29 22:41:52 +0300

    [CLANG] Use add memory santitzers

--- desktop_clang.mk
+++ desktop_clang.mk
@@ -1,5 +1,5 @@
 CC = clang
 LD = clang
 CFLAGS += $(shell pkg-config --cflags gtk+-2.0)
-CFLAGS += $(DFLAGS) -g -Ddebug  -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wcast-align #-Wconversion
-LDFLAGS += $(shell pkg-config --libs gtk+-2.0) -rdynamic
+CFLAGS += $(DFLAGS) -g -Ddebug  -Wsign-conversion -Wunreachable-code  -Wstrict-overflow=5 -Wcast-align -fsanitize=memory -fno-omit-frame-pointer -fno-optimize-sibling-calls -fsanitize-memory-track-origins=2 -fsanitize-memory-use-after-dtor -O0
+LDFLAGS +=  $(CFLAGS) $(shell pkg-config --libs gtk+-2.0) -rdynamic
